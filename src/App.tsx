1f2c49c4cae3d82517023269b3027ca332033cb6
Aman Singh <amansjot@gmail.com>
2023-10-12 16:38:46 -0400

    started the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import * as React from "react"
+import * as React from "react";
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
   VStack,
-  Code,
   Grid,
   theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+  Heading,
+  Image,
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+// import { Logo } from "./Logo";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
@@ -18,21 +17,11 @@ export const App = () => (
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
+          <Heading>CS + SG Project</Heading>
+          <Image src="logo-color.svg" w="500px" alt=""/>
+          {/* <Logo h="40vmin" pointerEvents="none" /> */}
         </VStack>
       </Grid>
     </Box>
   </ChakraProvider>
-)
+);
